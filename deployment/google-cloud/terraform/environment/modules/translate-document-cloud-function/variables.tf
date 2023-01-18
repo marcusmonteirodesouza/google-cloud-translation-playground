8c6092913c6ca8940c3f5dc8ff4a0ad263dcc304
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2023-01-17 20:56:34 -0500

    Fix deployment

--- deployment/google-cloud/terraform/environment/modules/translate-document-cloud-function/variables.tf
+++ deployment/google-cloud/terraform/environment/modules/translate-document-cloud-function/variables.tf
@@ -1,8 +1,3 @@
-variable "project_id" {
-  type        = string
-  description = "The project ID."
-}
-
 variable "region" {
   type        = string
   description = "The default GCP region for the created resources."
