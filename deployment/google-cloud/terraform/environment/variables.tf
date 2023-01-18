004d8d981af2e4f7cf915ff8869bb17a69166153
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2023-01-17 20:58:39 -0500

    Fix deployment

--- deployment/google-cloud/terraform/environment/variables.tf
+++ deployment/google-cloud/terraform/environment/variables.tf
@@ -1,3 +1,8 @@
+variable "project_id" {
+  type        = string
+  description = "The project ID."
+}
+
 variable "region" {
   type        = string
   description = "The default GCP region for the created resources."
